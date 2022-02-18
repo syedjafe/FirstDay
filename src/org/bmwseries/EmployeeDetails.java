ebd9431788b23b03fed55afca8d4a6ec5df792fe
syedjafe <syedjafer57@gmail.com>
2022-02-18 18:27:59 +0530

    MyTask

---
+++ src/org/bmwseries/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package org.bmwseries;
+
+public class EmployeeDetails {
+	public void data() {
+		long empId = 98768l;
+		System.out.println(empId);
+		String empName = "Karthick";
+		System.out.println(empName);
+		String empEmail = "karthick98@gmail.com";
+		System.out.println(empEmail);
+		long empPhoneno = 9842986541l;
+		System.out.println(empPhoneno);
+		int empSalary = 40000;
+		System.out.println(empSalary);
+		char empGender = 'M';
+		System.out.println(empGender);
+		String empCity = "Trichy";
+		System.out.println(empCity);
+		
+		System.out.println("Salary is"+empSalary);
+		System.out.println(empName+empGender);
+		System.out.println(empId+empPhoneno+empSalary);
+	}
+	public static void main(String[] args) {
+		EmployeeDetails e = new EmployeeDetails();
+		e.data();
+	}
+}
